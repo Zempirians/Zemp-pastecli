3ee438c0f94785428dbebfffe8d566a161822ce0
UberLame <w0rm@sploit.me.uk>
2013-12-09 09:41:43 -0600

    cleaned index.php, now its readable.

--- public_html/index.php
+++ public_html/index.php
@@ -1,15 +1,18 @@
 <?php 
-include('config.php'); 
-$config = new config();
-@$data= $_REQUEST['code'];
-if(!empty($data)){
-$url = $config->paste($data , $_SERVER['REMOTE_ADDR']);
-print  "link: http://".$_SERVER["SERVER_NAME"].$_SERVER['PHP_SELF']."?view_id=".$url."";
+	include('config.php'); 
+	$config = new config();
+	@$data= $_REQUEST['code'];
 
-}
-if(isset($_GET['view_id'])){
-$view= (int) $_GET['view_id'];
-$row = $config->view($view);
-echo 'Date posted: <b>',$row['date'],'</b><br/>'.'<div id=\'code\'><code>'.$row['paste'].'</code></div>';
-}?>
+	if(!empty($data)){
+		$url = $config->paste($data , $_SERVER['REMOTE_ADDR']);
+		print  "link: http://".$_SERVER["SERVER_NAME"].$_SERVER['PHP_SELF']."?view_id=".$url."";
+	}
+
+	if(isset($_GET['view_id'])){
+		$view= (int) $_GET['view_id'];
+		$row = $config->view($view);
+		echo 'Date posted: <b>',$row['date'],'</b><br/>'.'<div id=\'code\'><code>'.$row['paste'].'</code></div>';
+	}
+
+?>
 
